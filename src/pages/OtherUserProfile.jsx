7fb9d3366ec2848a6a52631d2208c6b1abe7388b
Raushan Kumar <raushankp75@gmail.com>
2023-06-27 19:44:42 +0530

    Follow and unfollow user functionality added in userProfile page

--- src/pages/OtherUserProfile.jsx
+++ src/pages/OtherUserProfile.jsx
@@ -11,11 +11,12 @@ const OtherUserProfile = () => {
     const [user, setUser] = useState('')
     const [post, setPost] = useState([])
 
-    // const [popup, setPopup] = useState(false)
-    // const [commentPopupItems, setCommentPopupItems] = useState([])
+    // for dynamic follow unfollow button
+    const [isFollow, setIsFollow] = useState(false)
 
 
 
+    // get user profile
     const getMyPost = () => {
         axios.get(`http://localhost:8000/user/${userid}`, {
             headers: {
@@ -27,6 +28,11 @@ const OtherUserProfile = () => {
             setUser(res.data.user)
             setPost(res.data.post)
 
+            /// conditionally follow and unfollow
+            if (res.data.user.followers.includes(JSON.parse(localStorage.getItem('user'))._id)) {
+                setIsFollow(true)
+            }
+
         }).catch((err) => {
             console.log('Signup Error 48: ', err.response.data)
         })
@@ -34,17 +40,73 @@ const OtherUserProfile = () => {
 
     useEffect(() => {
         getMyPost();
-    }, [])
+    }, [isFollow])
 
 
 
 
-    // handle popup
-    //   const handlePopup = (post) => {
-    //     setPopup(true)
-    //     setCommentPopupItems(post)
-    //     // console.log(146,commentPopupItems)
-    //   }
+
+    // follow users function
+    const followUser = (userId) => {
+
+        console.log(userId)
+
+        axios.put(`http://localhost:8000/user/follow`, { followId: userId }, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        }).then((res) => {
+            console.log(res.data)
+
+            // change follow unfollow conditionally
+            setIsFollow(true)
+            // if (res.data.success === true) {
+            //     setText('');
+            //     toast.success(res.data.message);
+            //     getAllPost()
+            //     // console.log(res.data.message)
+            // }
+        }).catch((err) => {
+            // if (err.code === 'ERR_BAD_REQUEST') {
+            console.log('Add commment error: ', err)
+            //     toast.error(err.response.data.error);
+            // }
+        })
+
+    }
+
+
+
+    // unfollow users function
+    const unfollowUser = (userId) => {
+
+        console.log(userId)
+
+        axios.put(`http://localhost:8000/user/unfollow`, { followId: userId }, {
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": "Bearer " + localStorage.getItem("token")
+            }
+        }).then((res) => {
+            console.log(res.data)
+
+            // change follow unfollow conditionally
+            setIsFollow(false)
+            // if (res.data.success === true) {
+            //     setText('');
+            //     toast.success(res.data.message);
+            //     getAllPost()
+            //     // console.log(res.data.message)
+            // }
+        }).catch((err) => {
+            // if (err.code === 'ERR_BAD_REQUEST') {
+            console.log('Add commment error: ', err)
+            //     toast.error(err.response.data.error);
+            // }
+        })
+
+    }
 
 
 
@@ -64,13 +126,31 @@ const OtherUserProfile = () => {
                                 sx={{ width: '140px', height: '140px', borderRadius: '50%' }}
                             />
                             <Box sx={{ display: 'flex', flexDirection: 'column', gap: '25px' }}>
-                                <Typography gutterBottom sx={{ margin: '6px 0', fontWeight: '600', fontSize: '30px' }}>
-                                    {user.name}
-                                </Typography>
+                                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '30px', alignItems: 'center' }}>
+                                    <Typography gutterBottom sx={{ margin: '6px 0', fontWeight: '600', fontSize: '30px' }}>{user.name}</Typography>
+
+                                    {/* conditionaliy render function on follow and unfollow */}
+                                    <Box>
+                                        <Button
+                                            onClick={() => {
+                                                if (isFollow) {
+                                                    unfollowUser(user._id)
+                                                } else {
+                                                    followUser(user._id)
+                                                }
+                                            }}
+                                            sx={{ textTransform: 'capitalize', border: '2px solid black', padding: '2px 20px' }}>
+                                            {isFollow ? 'Unfollow' : 'Follow'}
+                                        </Button>
+                                    </Box>
+                                </Box>
+
+
+                                {/* follow and following count */}
                                 <Box sx={{ display: 'flex', flexDirection: 'row', gap: '40px' }}>
-                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>15</Box> posts</Typography>
-                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>15</Box> followers</Typography>
-                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>15</Box> following</Typography>
+                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>{post.length}</Box> Post</Typography>
+                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>{user.followers ? user.followers.length : '0'}</Box> followers</Typography>
+                                    <Typography fontSize={16}><Box component='span' sx={{ fontWeight: 'bold' }}>{user.following ? user.following.length : '0'}</Box> following</Typography>
                                 </Box>
                             </Box>
                         </Box>
@@ -78,6 +158,7 @@ const OtherUserProfile = () => {
 
 
                         <hr />
+                        {/* post image view */}
                         <CardContent>
                             <Grid container spacing={0.3}>
                                 {post.map((post) => {
